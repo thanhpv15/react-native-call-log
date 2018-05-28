2609e4f8e4e2d1502a0f8ceb78aa9511b6dc469d
Thanh Phung <tank90.it@gmail.com>
2018-05-28 10:37:09 +0700

    add get call log by phone

--- android/src/main/java/com/wscodelabs/callLogs/CallLogModule.java
+++ android/src/main/java/com/wscodelabs/callLogs/CallLogModule.java
@@ -15,6 +15,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import java.util.Date;
 
 import java.util.Map;
 
@@ -85,6 +86,93 @@ public class CallLogModule extends ReactContextBaseJavaModule {
                     dir = "MISSED";
                     break;
             }
+
+            JSONObject callObj = new JSONObject();
+            try{
+                callObj.put("phoneNumber",phNumber);
+                callObj.put("callType", dir);
+                callObj.put("callDate", callDate);
+                callObj.put("callDuration", callDuration);
+                callObj.put("callDayTime", callDayTime);
+                callObj.put("cachedName", cachedName);
+                callObj.put("subscription", subscription);
+                if (hasSlotId) {
+                    callObj.put("slotId", simId);
+                }
+                callArray.put(callObj);
+            }
+            catch(JSONException e){
+                e.printStackTrace();
+            }
+
+
+        }
+        cursor.close();
+        callBack.invoke(callArray.toString());
+    }
+
+    @ReactMethod
+    public void getLog(String type, String phone, String startTime, Callback callBack) {
+        boolean hasSlotId = false;
+        Uri uri;
+        StringBuffer stringBuffer = new StringBuffer();
+        String deviceMan = android.os.Build.MANUFACTURER;
+        if (Build.VERSION_CODES.M <= Build.VERSION.SDK_INT && "samsung".equals(deviceMan)){
+            hasSlotId = true;
+            uri = Uri.parse("content://logs/call");
+        }else {
+            uri = CallLog.Calls.CONTENT_URI;
+        }
+        Cursor cursor = this.context.getContentResolver().query(uri,
+                null, null, null, CallLog.Calls.DATE + " DESC");
+
+        if (cursor == null) {
+            callBack.invoke("[]");
+            return;
+        }
+        int number = cursor.getColumnIndex(CallLog.Calls.NUMBER);
+        int type = cursor.getColumnIndex(CallLog.Calls.TYPE);
+        int date = cursor.getColumnIndex(CallLog.Calls.DATE);
+        int duration = cursor.getColumnIndex(CallLog.Calls.DURATION);
+        int name = cursor.getColumnIndex(CallLog.Calls.CACHED_NAME);
+        int subIndex = cursor.getColumnIndex("subscription_id");
+        int simIndex = cursor.getColumnIndex(hasSlotId ? "sim_id" : "simid");
+
+        JSONArray callArray = new JSONArray();
+        while (cursor.moveToNext()) {
+            String phNumber = cursor.getString(number);
+            String callType = cursor.getString(type);
+            String callDate = cursor.getString(date);
+            Date callDayTime = new Date(Long.valueOf(callDate));
+            String callDuration = cursor.getString(duration);
+            String cachedName = cursor.getString(name);
+            String sub = subIndex == -1 ? null : cursor.getString(subIndex);
+            String simId = simIndex == -1 ? null : cursor.getString(simIndex);
+            String subscription = (sub == null || "".equals(sub))? simId: sub;
+            subscription = subscription == null ? "" : subscription;
+            String dir = null;
+            int dircode = Integer.parseInt(callType);
+            switch (dircode) {
+                case CallLog.Calls.OUTGOING_TYPE:
+                    dir = "OUTGOING";
+                    break;
+                case CallLog.Calls.INCOMING_TYPE:
+                    dir = "INCOMING";
+                    break;
+
+                case CallLog.Calls.MISSED_TYPE:
+                    dir = "MISSED";
+                    break;
+            }
+
+            if (phNumber.equals(phone)) continue;
+            if (dir.equals(type)) continue;
+
+            long logTime = Long.parseLong(callDate);
+            long start = Long.parseLong(startTime);
+            Date startDate = new Date(start);
+            Date logDate = new Date(logTime);
+            if (startDate.compareTo(logDate) > 0) continue;
 
             JSONObject callObj = new JSONObject();
             try{
